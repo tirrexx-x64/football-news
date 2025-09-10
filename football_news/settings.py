27c5f2c542ebe8405ec1d84a3e3f6161b5df9d1c
tirrexx-x64 <tirtarendysi77@gmail.com>
2025-09-10 15:21:24 +0700

    final tutorial 2:

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,6 +33,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","tirta-rendy-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://tirta-rendy-footballnews.pbp.cs.ui.ac.id"
+]
+
 
 # Application definition
 
@@ -61,7 +65,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
